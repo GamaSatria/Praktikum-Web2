df5b2f0fe2be15caed59de14587216ce177ad5a0
GamaSatria <mgama6969@gmail.com>
2023-05-21 08:47:13 +0700

    Praktikum7

---
+++ Praktikum07/Tugas/persegi_panjang.php
@@ -0,0 +1,16 @@
+<?php  
+class persegiPanjang {
+    public $panjang, $lebar; 
+    function __construct($panjang, $lebar) {
+        $this->$panjang = $panjang;
+        $this->$lebar = $lebar;
+    }
+    function panjang() {
+        return $this->panjang;
+    }
+    function lebar() {
+        return $this->lebar;
+    }
+    
+}
+?>
